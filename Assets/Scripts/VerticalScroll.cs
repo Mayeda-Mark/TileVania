e3bc187cc362cd5f84a0a6a8ab4399054043892d
JapaneseFireBmb <markrmayeda@gmail.com>
2020-06-17 16:21:31 -0700

    added rising water

---
+++ Assets/Scripts/VerticalScroll.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalScroll : MonoBehaviour {
+    [SerializeField] float scrollRate = 1.2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float yMove = scrollRate * Time.deltaTime;
+        transform.Translate(new Vector2(0f, yMove));
+	}
+}
